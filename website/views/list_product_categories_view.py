824a3649c08469a4fd105f05ba3ccf614a36eff4
William A Sims III <willsims14@gmail.com>
2017-05-20 13:10:11 -0500

    Added generic page titles to list.html

--- website/views/list_product_categories_view.py
+++ website/views/list_product_categories_view.py
@@ -5,4 +5,4 @@ from website.models.product_model import ProductCategory
 def list_product_categories(request):
     all_product_categories = ProductCategory.objects.all()
     template_name = 'product/list.html'
-    return render(request, template_name, {'things': all_product_categories})
+    return render(request, template_name, {'things': all_product_categories, "page_title": "Product Categories"})
--- website/views/list_products_view.py
+++ website/views/list_products_view.py
@@ -5,4 +5,4 @@ from website.models.product_model import Product
 def list_products(request):
     all_products = Product.objects.all()
     template_name = 'product/list.html'
-    return render(request, template_name, {'things': all_products})
+    return render(request, template_name, {'things': all_products, "page_title":"Products"})
