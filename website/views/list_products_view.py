dee6c5150cdcde119c3f924a7130ed83610ebca1
William A Sims III <willsims14@gmail.com>
2017-05-22 09:57:02 -0500

    Removed /static/ from gitignore and changed variable names

--- website/views/list_products_view.py
+++ website/views/list_products_view.py
@@ -5,4 +5,4 @@ from website.models.product_model import Product
 def list_products(request):
     all_products = Product.objects.all()
     template_name = 'product/list.html'
-    return render(request, template_name, {'things': all_products, "page_title":"Products"})
+    return render(request, template_name, {'items': all_products, "page_title":"Products"})
