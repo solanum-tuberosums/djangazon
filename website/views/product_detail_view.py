567ff2e02a650ae8629bca30d1a90b78758a77f2
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-05-22 15:57:08 -0500

    trying to connect add-to-cart button and action

--- website/views/product_detail_view.py
+++ website/views/product_detail_view.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from website.models.product_model import Product
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 def product_detail(request, product_id):
 	"""
@@ -11,17 +12,37 @@ def product_detail(request, product_id):
 
 	Author: Will Sims
 	"""
-
-	template_name = 'product/detail.html'
-	product = Product.objects.get(pk=product_id)
-
-	# Get seller object
-	seller = User.objects.get(pk=product.seller_id)
-	seller_name = " ".join([seller.first_name.title(), seller.last_name.title()])
-
-	# This part can probably be refactored, I just wanted to get something that works merged in
-	# (manually extracting the product data and creating a list of tuples is probably resource expensive)
-	new_product = [("Seller", seller_name), ("Description", product.description), ("Price", product.price), ("Quantity", product.quantity), ("Date Listed", product.date_added)]
-
 	
-	return render(request, template_name, {'object_to_display': new_product, "page_title":product.title})
+	if request.method == 'GET':
+
+		template_name = 'product/detail.html'
+		product = Product.objects.get(pk=product_id)
+
+		# Get seller object
+		seller = User.objects.get(pk=product.seller_id)
+		seller_name = " ".join([seller.first_name.title(), seller.last_name.title()])
+
+		# This part can probably be refactored, I just wanted to get something that works merged in
+		# (manually extracting the product data and creating a list of tuples is probably resource expensive)
+		new_product = [("Seller", seller_name), ("Description", product.description), ("Price", product.price), ("Quantity", product.quantity), ("Date Listed", product.date_added)]
+
+		
+		return render(request, template_name, {'object_to_display': new_product, "page_title":product.title, "type": "product", "product_id": product.pk})
+
+	elif request.method == 'POST':
+		print('post')
+		o = Order(
+			date = timezone.now(),
+			payment_type = None,
+			profile_id = request.user
+			)
+		o.save()
+		order_pk = Order.objects.latest('id')
+		po = ProductOrder(
+			order_id = order_pk,
+			product_id = Product.objects.get(pk=product_id)
+    	)
+		po.save()
+		template_name = 'index.html'
+		#should redirect back to list of payment types
+		return render(request, template_name, {})
