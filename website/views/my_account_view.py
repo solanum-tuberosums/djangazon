b804bc3c9238f1242179d559e7556651b5984faa
William A Sims III <willsims14@gmail.com>
2017-05-23 14:35:08 -0500

    User can view their payment types in my account page

--- website/views/my_account_view.py
+++ website/views/my_account_view.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 
 from django.contrib.auth.models import User
+from website.models import PaymentType
 
 def my_account(request, user_id):
-	"""
-	This function renders the request using:
-		- TEMPLATE: my_account.html
+    """
+    This function renders the request using:
+    - TEMPLATE: my_account.html
 
-	Author: Blaise Roberts
-	"""
-	
-	template_name = 'my_account.html'
-
-	
-	return render(request, template_name, {})
+    Author: Blaise Roberts
+    """
+  
+    template_name = 'my_account.html'
+    payment_types = PaymentType.objects.filter(cardholder=user_id)
+  
+    return render(request, template_name, {"payment_types": payment_types})
