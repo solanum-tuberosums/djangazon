e68b7878ad7ac6db16e7c2f06b30f2a62712de80
William A Sims III <willsims14@gmail.com>
2017-05-23 11:27:13 -0500

    Added comments to view

--- website/views/list_product_categories_view.py
+++ website/views/list_product_categories_view.py
@@ -21,18 +21,20 @@ def list_product_categories(request):
 
 	# For each category
 	for category in all_product_categories:
-		# Get top 3 proeducrs
+		# Get top 3 products for this category
 		temp_top_three_products = Product.objects.filter(product_category=category.id).order_by('-id')[:3]
-		# Make a list
+		# Make a placeholder list that will hold two indexed values
+		#	- product_id
+		#	- product_title
 		placeholder_list = list()
 		# For each product in each category:
 		for product in temp_top_three_products:
 			# Add each the top 3 products to a placeholder list
 			placeholder_list.append((product.id, product.title))
-
-		# Get count for each category
+		# Get count for this category
 		this_category_product_count = category.category_products.count()
-		# After the second loop, create a tuple with a length of 2 (0 = CategoryName, 1 = Top3Products)
+
+		# After the second loop, create a tuple with a length of 3 (0 = CategoryName, 1 = placeholder_list, 2 = count of products)
 		final_three_products.append((category, placeholder_list, this_category_product_count))
 
 
