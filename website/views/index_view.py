c7590c5802696c8b08683aa358ae7b9297abf5d3
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-05-23 14:32:41 -0500

    initial commit

--- website/views/index_view.py
+++ website/views/index_view.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from website.models.order_model import Order
 
+from website.models.product_model import Product
+
 def index(request):
 	try:
 		order = Order.objects.get(user=request.user)
 	except:
 		order = 'fail'
-	template_name = 'index.html'
-	return render(request, template_name, {'order':order})
+    template_name = 'index.html'
+    products = Product.objects.all()
+    return render(request, template_name, {'order':order, 'products': products})
