735bc31d8d062d06fda395496e11103a9c588991
William A Sims III <willsims14@gmail.com>
2017-05-26 09:09:44 -0500

    Imported models in admin

--- website/admin.py
+++ website/admin.py
@@ -2,11 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product, ProductCategory
+from .models import Product, ProductCategory, PaymentType, Order, ProductOrder
 
 admin.site.register(Product)
 admin.site.register(ProductCategory)
 admin.site.register(PaymentType)
-admin.site.register(User)
 admin.site.register(Order)
 admin.site.register(ProductOrder)
