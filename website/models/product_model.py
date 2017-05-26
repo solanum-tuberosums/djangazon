dfbfc7539f57c83d11551c2be02406473c5c71bf
William A Sims III <willsims14@gmail.com>
2017-05-26 15:20:52 -0500

    Wrote new method and reformatted product categories page

--- website/models/product_model.py
+++ website/models/product_model.py
@@ -45,3 +45,6 @@ class Product (models.Model):
     def formatted_price_no_decimals(self):
         temp_price = locale.currency(self.price, grouping=True)
         return temp_price[:-3]
+
+    def seller_string(self):
+        return " ".join([self.seller.first_name, self.seller.last_name])
