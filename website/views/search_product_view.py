fa12fa722c34298fb9646e8083ef122dfe9e6df0
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-05-30 11:10:35 -0500

    search functionality works for title and description

---
+++ website/views/search_product_view.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.db.models import Q
+from website.models.product_model import Product
+
+
+def search_products(request):
+    """
+    This function is invoked to search products available for sale.
+
+    ---Arguments---
+    request: the full HTTP request object
+
+    ---GET---
+    Renders list.html
+
+        ---Context---
+        'items'(QuerySet): searched_products, 
+        'page_title'(string): A string, "Products", identifying the title of the page.
+
+    Author: Jeremy Bakker
+    """
+    if request.method == 'GET':
+        form_data = request.GET
+        search_box = form_data['search_box']
+        all_products = Product.objects.filter(Q(title__icontains=search_box) | Q(description__icontains=search_box))
+        template_name = 'list.html'
+        return render(request, template_name, {'items': all_products, 
+            "page_title":"Products", "error": "Search Query Returned No Results"})
