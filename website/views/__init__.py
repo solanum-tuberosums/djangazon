517ab325b70d0840a7c77c7b295a3121d57f03e0
William A Sims III <willsims14@gmail.com>
2017-05-31 10:04:59 -0500

    Setup remove product for sale button and logic

--- website/views/__init__.py
+++ website/views/__init__.py
@@ -20,3 +20,4 @@ from website.views.complete_order_view import complete_order
 from website.views.delete_order_view import delete_order
 from website.views.order_history_view import order_history
 from website.views.search_product_view import search_products
+from website.views.remove_product_view import remove_product
--- website/views/product_detail_view.py
+++ website/views/product_detail_view.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
 from django.utils import timezone
 from django.contrib.auth.models import User
@@ -8,10 +9,10 @@ from website.models.product_order_model import ProductOrder
 
 
 def product_detail(request, product_id):
-	"""
-	This function renders the product deatils and either adds the product to 
-	an open order or creates a new order and add the product to the newly
-	created order.
+    """
+    This function renders the product deatils and either adds the product to
+    an open order or creates a new order and add the product to the newly
+    created order.
 
     ---Arguments---
     None
@@ -20,42 +21,61 @@ def product_detail(request, product_id):
     Renders detail.html
 
         ---Context---
-        'product': The Product that was clicked on is the data that this view 
-			returns
+        'product': The Product that was clicked on is the data that this view
+            returns
 
     ---POST---
     Renders success/product_added_to_cart_links.html
 
-    	---Context---
+        ---Context---
         'posted_object': 'Product Added to Cart'
         'posted_object_identifier': The title of the selected product
 
-	Author: Will Sims & Blaise Roberts
+    Author: Will Sims & Blaise Roberts
     """
 
-	if request.method == 'GET':
-		template_name = 'detail.html'
-		product = Product.objects.get(pk=product_id)
-		return render(request, template_name, {'product': product})
+    current_users_product = False
 
-	elif request.method == 'POST':
-		product = Product.objects.get(pk=product_id)
-		try:
-			order = Order.objects.get(user=request.user, payment_type=None)
-		except Order.DoesNotExist:
-			order = Order(
-				order_date = timezone.now(),
-				payment_type = None,
-				user = request.user
-				)
-			order.save()
-		po = ProductOrder(
-			order = order,
-			product = product
-		)
-		po.save()
-		template_name = 'success/product_added_to_cart_links.html'
-		
-		return render(request, template_name, {
-			'posted_object': 'Product Added to Cart', 
-			'posted_object_identifier': product.title})
+    if request.method == 'GET':
+        template_name = 'detail.html'
+        product = Product.objects.get(pk=product_id)
+
+        if product.seller == request.user:
+            current_users_product = True
+
+
+
+        return render(request, template_name, {'product': product, "current_users_product": current_users_product})
+
+    elif request.method == 'POST':
+
+        button_clicked = request.POST.get("detail_button", "")
+
+        if button_clicked.lower() == "add":
+            product = Product.objects.get(pk=product_id)
+            try:
+                order = Order.objects.get(user=request.user, payment_type=None)
+            except Order.DoesNotExist:
+                order = Order(
+                    order_date = timezone.now(),
+                    payment_type = None,
+                    user = request.user
+                    )
+                order.save()
+
+            po = ProductOrder(
+                order = order,
+                product = product
+            )
+            po.save()
+            template_name = 'success/product_added_to_cart_links.html'
+
+            return render(request, template_name, {
+                'posted_object': 'Product Added to Cart',
+                'posted_object_identifier': product.title})
+        elif button_clicked.lower() == 'delete':
+            print("DELETEING")
+            template_name = 'success/success.html'
+            return render(request, template_name, {"posted_object":product_id})
+
+
