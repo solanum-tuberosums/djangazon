675c3a372854279458cd0e509550b8aac4bd3ead
William A Sims III <willsims14@gmail.com>
2017-06-01 13:56:21 -0500

    User can view order history, but counts for duplicate products is not working yet

--- website/views/order_history_view.py
+++ website/views/order_history_view.py
@@ -1,13 +1,24 @@
+from django.http import HttpResponseForbidden
 from django.shortcuts import render
 from website.models import Order, Profile
-from django.http import HttpResponseForbidden
 
-def order_history(request, user_id):
+def order_history(request):
+
+    if request.user:
+        profile = Profile.objects.get(pk=request.user.id)
+
+        # user_orders = Order.objects.filter(user=request.user)
+        user_completed_orders = Order.objects.filter(user=request.user).exclude(payment_type__isnull=True)
+
+        print("\n")
+        for order in user_completed_orders:
+            print('\nOrder:')
 
 
-    if str(request.user.id) == str(user_id):
-        profile = Profile.objects.get(pk=user_id)
-        return render(request, 'order_history.html', {"profile":profile})
+
+        return render(request, 'order_history.html', {"profile":profile, 'orders':user_completed_orders})
     else:
         return HttpResponseForbidden('''<h1>Not your account, homie.</h1>
             <img src="/website/static/other.jpg">''')
+
+
