77b452c46f8b38a73f0becf5c99cf62c342699b0
Jessica Younker <jessica.younker@gmail.com>
2017-06-01 15:11:54 -0500

    added blank=true and null=true to profile model fields

--- website/models/profile_model.py
+++ website/models/profile_model.py
@@ -27,11 +27,11 @@ class Profile (models.Model):
     """
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    street_address = models.CharField(max_length=100)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=20)
-    postal_code = models.CharField(max_length=20)
-    phone = models.CharField(max_length=18)
+    street_address = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    state = models.CharField(max_length=20, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, blank=True, null=True)
+    phone = models.CharField(max_length=18, blank=True, null=True)
 
     def get_user_order(self):
         """
