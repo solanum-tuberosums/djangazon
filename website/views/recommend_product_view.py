366916d99641201000cf9044ac68be3d408fc6e1
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-06-01 14:56:15 -0500

    user recommendation now populates database

--- website/views/recommend_product_view.py
+++ website/views/recommend_product_view.py
@@ -4,6 +4,8 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.models import User
 from website.models.product_model import Product
+from website.models.profile_model import Profile
+from website.models.user_product_recommendation_model import UserProductRecommendation
 
 def recommend_product(request, product_id):
     product = Product.objects.get(pk=product_id)
@@ -14,6 +16,17 @@ def recommend_product(request, product_id):
     if request.method=="POST":
         form_data = request.POST
         user = form_data['user_search_box']
+        sender = request.user
+        receiver_user_instance = User.objects.get(first_name = user)
+        receiver_profile_instance = Profile.objects.get(user = receiver_user_instance)
+        upr = UserProductRecommendation(
+                viewed = False,
+                product = product,
+                receiver = receiver_profile_instance,
+                sender = sender
+            )
+        upr.save()
+
         template = 'detail.html'
         
         return HttpResponseRedirect(reverse('website:product_detail', args=[product.id]))
