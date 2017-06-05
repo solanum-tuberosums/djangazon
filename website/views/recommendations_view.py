40658ec7490eca58552f6206a05991a2098cadf3
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-06-05 09:21:10 -0500

    adjusted code to ruler

--- website/views/recommendations_view.py
+++ website/views/recommendations_view.py
@@ -5,7 +5,8 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from website.models.product_model import Product
 from website.models.profile_model import Profile
-from website.models.user_product_recommendation_model import UserProductRecommendation
+from website.models.user_product_recommendation_model import \
+    UserProductRecommendation
 
 
 def recommendations(request, user_id):
@@ -26,8 +27,10 @@ def recommendations(request, user_id):
     """
     
     receiver_user_instance = User.objects.get(id = user_id)
-    receiver_profile_instance = Profile.objects.get(user = receiver_user_instance)
-    upr_products = UserProductRecommendation.objects.filter(receiver=receiver_profile_instance, viewed=False)
+    receiver_profile_instance = Profile.objects.get(user = 
+        receiver_user_instance)
+    upr_products = UserProductRecommendation.objects.filter(receiver=
+        receiver_profile_instance, viewed=False)
     product_id_list = []
     for product in upr_products:
         product_id_list.append(product.product_id)
@@ -40,4 +43,5 @@ def recommendations(request, user_id):
 
     template='list.html'
 
-    return render(request, template, {"items":product_instance_list, "error": "You have no current recommendations."})
+    return render(request, template, {"items":product_instance_list, 
+        "error": "You have no current recommendations."})
